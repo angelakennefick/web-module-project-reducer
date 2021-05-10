04c9dc7369ec887ac0713f6bf6011d94a1ce4563
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-05-10 17:56:54 -0500

    1up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, {useState, useReducer} from 'react';
+import React, {useReducer} from 'react';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
-import reducer, {initialState} from './reducers/index'
+
+import reducer, {initialState} from './reducers/index';
+
+import {addOne} from './actions/index';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -34,7 +37,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={() => dispatch(addOne())}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
